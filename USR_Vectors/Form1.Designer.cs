688547605b661fa424a5debd7ff63d0f50e1c5b7
Олег Гримуто <o.grimuto@gmail.com>
2020-12-22 19:43:14 +0300

    Добавьте файлы проекта.

---
+++ USR_Vectors/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace USR_Vectors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.radioButtonPlus = new System.Windows.Forms.RadioButton();
+            this.radioButtonMinus = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.aLabel = new System.Windows.Forms.Label();
+            this.bLabel = new System.Windows.Forms.Label();
+            this.cLabel = new System.Windows.Forms.Label();
+            this.coordXLabel = new System.Windows.Forms.Label();
+            this.coordYLabel = new System.Windows.Forms.Label();
+            this.lengthLabel = new System.Windows.Forms.Label();
+            this.calc_button = new System.Windows.Forms.Button();
+            this.clear_button = new System.Windows.Forms.Button();
+            this.pbALabel = new System.Windows.Forms.Label();
+            this.pbBLabel = new System.Windows.Forms.Label();
+            this.pbCLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(362, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(64, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(442, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(64, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(521, 27);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(73, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(362, 53);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(64, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(442, 53);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(64, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(521, 53);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(73, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Enabled = false;
+            this.textBox7.Location = new System.Drawing.Point(362, 79);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(64, 20);
+            this.textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Enabled = false;
+            this.textBox8.Location = new System.Drawing.Point(442, 79);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(64, 20);
+            this.textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Enabled = false;
+            this.textBox9.Location = new System.Drawing.Point(521, 79);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(73, 20);
+            this.textBox9.TabIndex = 8;
+            // 
+            // radioButtonPlus
+            // 
+            this.radioButtonPlus.AutoSize = true;
+            this.radioButtonPlus.Location = new System.Drawing.Point(345, 239);
+            this.radioButtonPlus.Name = "radioButtonPlus";
+            this.radioButtonPlus.Size = new System.Drawing.Size(76, 17);
+            this.radioButtonPlus.TabIndex = 9;
+            this.radioButtonPlus.TabStop = true;
+            this.radioButtonPlus.Text = "Сложение";
+            this.radioButtonPlus.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMinus
+            // 
+            this.radioButtonMinus.AutoSize = true;
+            this.radioButtonMinus.Location = new System.Drawing.Point(345, 263);
+            this.radioButtonMinus.Name = "radioButtonMinus";
+            this.radioButtonMinus.Size = new System.Drawing.Size(80, 17);
+            this.radioButtonMinus.TabIndex = 10;
+            this.radioButtonMinus.TabStop = true;
+            this.radioButtonMinus.Text = "Вычитание";
+            this.radioButtonMinus.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // aLabel
+            // 
+            this.aLabel.AutoSize = true;
+            this.aLabel.Location = new System.Drawing.Point(341, 34);
+            this.aLabel.Name = "aLabel";
+            this.aLabel.Size = new System.Drawing.Size(14, 13);
+            this.aLabel.TabIndex = 12;
+            this.aLabel.Text = "A";
+            // 
+            // bLabel
+            // 
+            this.bLabel.AutoSize = true;
+            this.bLabel.Location = new System.Drawing.Point(341, 60);
+            this.bLabel.Name = "bLabel";
+            this.bLabel.Size = new System.Drawing.Size(14, 13);
+            this.bLabel.TabIndex = 13;
+            this.bLabel.Text = "B";
+            // 
+            // cLabel
+            // 
+            this.cLabel.AutoSize = true;
+            this.cLabel.Location = new System.Drawing.Point(341, 86);
+            this.cLabel.Name = "cLabel";
+            this.cLabel.Size = new System.Drawing.Size(14, 13);
+            this.cLabel.TabIndex = 14;
+            this.cLabel.Text = "C";
+            // 
+            // coordXLabel
+            // 
+            this.coordXLabel.AutoSize = true;
+            this.coordXLabel.Location = new System.Drawing.Point(383, 11);
+            this.coordXLabel.Name = "coordXLabel";
+            this.coordXLabel.Size = new System.Drawing.Size(14, 13);
+            this.coordXLabel.TabIndex = 15;
+            this.coordXLabel.Text = "X";
+            // 
+            // coordYLabel
+            // 
+            this.coordYLabel.AutoSize = true;
+            this.coordYLabel.Location = new System.Drawing.Point(466, 9);
+            this.coordYLabel.Name = "coordYLabel";
+            this.coordYLabel.Size = new System.Drawing.Size(14, 13);
+            this.coordYLabel.TabIndex = 16;
+            this.coordYLabel.Text = "Y";
+            // 
+            // lengthLabel
+            // 
+            this.lengthLabel.AutoSize = true;
+            this.lengthLabel.Location = new System.Drawing.Point(538, 11);
+            this.lengthLabel.Name = "lengthLabel";
+            this.lengthLabel.Size = new System.Drawing.Size(40, 13);
+            this.lengthLabel.TabIndex = 17;
+            this.lengthLabel.Text = "Длина";
+            // 
+            // calc_button
+            // 
+            this.calc_button.Location = new System.Drawing.Point(442, 248);
+            this.calc_button.Name = "calc_button";
+            this.calc_button.Size = new System.Drawing.Size(75, 23);
+            this.calc_button.TabIndex = 18;
+            this.calc_button.Text = "Расчет";
+            this.calc_button.UseVisualStyleBackColor = true;
+            this.calc_button.Click += new System.EventHandler(this.calc_button_Click);
+            // 
+            // clear_button
+            // 
+            this.clear_button.Location = new System.Drawing.Point(531, 248);
+            this.clear_button.Name = "clear_button";
+            this.clear_button.Size = new System.Drawing.Size(75, 23);
+            this.clear_button.TabIndex = 19;
+            this.clear_button.Text = "Очистка";
+            this.clear_button.UseVisualStyleBackColor = true;
+            this.clear_button.Click += new System.EventHandler(this.clear_button_Click);
+            // 
+            // pbALabel
+            // 
+            this.pbALabel.AutoSize = true;
+            this.pbALabel.BackColor = System.Drawing.SystemColors.Control;
+            this.pbALabel.ForeColor = System.Drawing.Color.Blue;
+            this.pbALabel.Location = new System.Drawing.Point(341, 144);
+            this.pbALabel.Name = "pbALabel";
+            this.pbALabel.Size = new System.Drawing.Size(14, 13);
+            this.pbALabel.TabIndex = 20;
+            this.pbALabel.Text = "A";
+            this.pbALabel.Visible = false;
+            // 
+            // pbBLabel
+            // 
+            this.pbBLabel.AutoSize = true;
+            this.pbBLabel.ForeColor = System.Drawing.Color.Blue;
+            this.pbBLabel.Location = new System.Drawing.Point(341, 177);
+            this.pbBLabel.Name = "pbBLabel";
+            this.pbBLabel.Size = new System.Drawing.Size(14, 13);
+            this.pbBLabel.TabIndex = 21;
+            this.pbBLabel.Text = "B";
+            this.pbBLabel.Visible = false;
+            // 
+            // pbCLabel
+            // 
+            this.pbCLabel.AutoSize = true;
+            this.pbCLabel.ForeColor = System.Drawing.Color.Red;
+            this.pbCLabel.Location = new System.Drawing.Point(341, 205);
+            this.pbCLabel.Name = "pbCLabel";
+            this.pbCLabel.Size = new System.Drawing.Size(14, 13);
+            this.pbCLabel.TabIndex = 22;
+            this.pbCLabel.Text = "C";
+            this.pbCLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(612, 311);
+            this.Controls.Add(this.pbCLabel);
+            this.Controls.Add(this.pbBLabel);
+            this.Controls.Add(this.pbALabel);
+            this.Controls.Add(this.clear_button);
+            this.Controls.Add(this.calc_button);
+            this.Controls.Add(this.lengthLabel);
+            this.Controls.Add(this.coordYLabel);
+            this.Controls.Add(this.coordXLabel);
+            this.Controls.Add(this.cLabel);
+            this.Controls.Add(this.bLabel);
+            this.Controls.Add(this.aLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.radioButtonMinus);
+            this.Controls.Add(this.radioButtonPlus);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Векторы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.RadioButton radioButtonPlus;
+        private System.Windows.Forms.RadioButton radioButtonMinus;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label aLabel;
+        private System.Windows.Forms.Label bLabel;
+        private System.Windows.Forms.Label cLabel;
+        private System.Windows.Forms.Label coordXLabel;
+        private System.Windows.Forms.Label coordYLabel;
+        private System.Windows.Forms.Label lengthLabel;
+        private System.Windows.Forms.Button calc_button;
+        private System.Windows.Forms.Button clear_button;
+        private System.Windows.Forms.Label pbALabel;
+        private System.Windows.Forms.Label pbBLabel;
+        private System.Windows.Forms.Label pbCLabel;
+    }
+}
---
+++ USR_Vectors/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USR_Vectors
+{
+    public partial class Form1 : Form
+    {
+        public double _aX, _aY, _aL, _bX, _bY, _bL, _cX, _cY, _cL;
+        public int _fullH, _fullW, _x0, _y0, _dtab = 25;
+
+        private void clear_button_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            _fullH = pictureBox1.Height;
+            _fullW = pictureBox1.Width;
+            Brush Pen = new SolidBrush(Color.White);
+            g.FillRectangle(Pen, 0, 0, _fullW, _fullH);
+            _x0 = 0;
+            _y0 = 0;
+            pbALabel.Visible = false; pbALabel.BackColor = Color.White;
+            pbBLabel.Visible = false; pbBLabel.BackColor = Color.White;
+            pbCLabel.Visible = false; pbCLabel.BackColor = Color.White;
+        }
+
+        public void Calculate()
+        {
+            if (radioButtonPlus.Checked == true)
+            {
+                _cX = _aX + _bX;
+                _cY = _aY + _bY;
+            }
+            if (radioButtonMinus.Checked == true)
+            {
+                _cX = _aX - _bX;
+                _cY = _aY - _bY;
+            }
+            _aL = Math.Sqrt(_aX * _aX + _aY * _aY);
+            _bL = Math.Sqrt(_bX * _bX + _bY * _bY);
+            _cL = Math.Sqrt(_cX * _cX + _cY * _cY);
+        }
+
+        private void calc_button_Click(object sender, EventArgs e)
+        {
+            DrawAxis();
+            DrawTable();
+            Input();
+            Calculate();
+            Output();
+            DrawVector();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void DrawAxis()
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            _fullH = pictureBox1.Height;
+            _fullW = pictureBox1.Width;
+
+            g.DrawLine(new Pen(Brushes.Black, 2), new Point(0, _fullH / 2), new Point(_fullW, _fullH / 2));
+            g.DrawLine(new Pen(Brushes.Black, 2), new Point(_fullW / 2, 0), new Point(_fullW / 2, _fullH));
+        }
+        public void DrawTable()
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            _fullH = pictureBox1.Height;
+            _fullW = pictureBox1.Width;
+            while (_x0 <= _fullW)
+            {
+                g.DrawLine(new Pen(Brushes.Black, 0.5f), new Point(_x0, 0), new Point(_x0, _fullH));
+                _x0 += _dtab;
+            }
+            while (_y0 <= _fullH)
+            {
+                g.DrawLine(new Pen(Brushes.Black, 0.5f), new Point(0, _y0), new Point(_fullW, _y0));
+                _y0 += _dtab;
+            }
+        }
+        public void Input()
+        {
+            try
+            {
+                _aX = Convert.ToDouble(textBox1.Text);
+                _aY = Convert.ToDouble(textBox2.Text);
+                _bX = Convert.ToDouble(textBox4.Text);
+                _bY = Convert.ToDouble(textBox5.Text);
+            }
+            catch
+            {
+
+            }
+
+        }
+        public void Output()
+        {
+            textBox3.Text = Convert.ToString(Math.Round(_aL, 2));
+            textBox6.Text = Convert.ToString(Math.Round(_bL, 2));
+            textBox9.Text = Convert.ToString(Math.Round(_cL, 2));
+            textBox7.Text = Convert.ToString(_cX);
+            textBox8.Text = Convert.ToString(_cY);
+        }                                       
+        public void DrawVector()
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            _fullH = pictureBox1.Height;
+            _fullW = pictureBox1.Width;
+            Point center, a, b, c;
+            center = new Point(_fullW / 2, _fullH / 2);
+            a = new Point(_fullW / 2 + Convert.ToInt32(Math.Round(_aX)),
+                _fullH / 2 + Convert.ToInt32(Math.Round(-_aY)));
+            b = new Point(_fullW / 2 + Convert.ToInt32(Math.Round(_bX)),
+                _fullH / 2 + Convert.ToInt32(Math.Round(-_bY)));
+            g.DrawLine(new Pen(Brushes.Blue, 2), center, a);
+            pbALabel.Location = new Point(_fullW / 2 + 10 + Convert.ToInt32(Math.Round(_aX)),
+                _fullH / 2 + 10 + Convert.ToInt32(Math.Round(-_aY))); pbALabel.Visible = true;
+            g.DrawLine(new Pen(Brushes.Blue, 2), center, b);
+            pbBLabel.Location = new Point(_fullW / 2 + 10 + Convert.ToInt32(Math.Round(_bX)),
+                _fullH / 2 + 10 + Convert.ToInt32(Math.Round(-_bY))); pbBLabel.Visible = true;
+            if (radioButtonPlus.Checked == true)
+            {
+                c = new Point(_fullW / 2 + Convert.ToInt32(Math.Round(_cX)),
+                _fullH / 2 + Convert.ToInt32(Math.Round(-_cY)));
+                g.DrawLine(new Pen(Brushes.Red, 2), center, c);
+                pbCLabel.Location = new Point(_fullW / 2 + 10 + Convert.ToInt32(Math.Round(_cX)),
+                _fullH / 2 + 10 + Convert.ToInt32(Math.Round(-_cY))); pbCLabel.Visible = true;
+            }
+            if (radioButtonMinus.Checked == true)
+            {
+                g.DrawLine(new Pen(Brushes.Red, 2), a, b);
+                pbCLabel.Location = new Point(_fullW / 2 - 10 + Convert.ToInt32(Math.Round(_bX)),
+                _fullH / 2 + 20 + Convert.ToInt32(Math.Round(-_bY))); pbCLabel.Visible = true;
+            }
+        }
+    }
+}
